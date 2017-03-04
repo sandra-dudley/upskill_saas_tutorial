3f5ac93fe36589f4e6cecc4da47da735ac2e8800
Sandra Dudley <sandra.dudley@ymail.com>
2017-03-04 11:33:00 +0000

    Installed SendGrid addon

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,22 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
+#ActionMailer::Base.smtp_settings = {
+#  :port           => ENV['MAILGUN_SMTP_PORT'],
+#  :address        => ENV['MAILGUN_SMTP_SERVER'],
+#  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+#  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+#  :domain         => 'protected-shelf-77246.herokuapp.com',
+#  :authentication => :plain,
+#}
+
 ActionMailer::Base.smtp_settings = {
-  :port           => ENV['MAILGUN_SMTP_PORT'],
-  :address        => ENV['MAILGUN_SMTP_SERVER'],
-  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :port           => 587,
+  :address        => 'smtp.sendgrid.net',
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
   :domain         => 'protected-shelf-77246.herokuapp.com',
   :authentication => :plain,
+  :enable_starttls_auto => true
 }
 ActionMailer::Base.delivery_method = :smtp
